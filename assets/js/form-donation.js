13c96f8745fdc16d063a95155399e4ee9a5818f9
fernando.perez <fernandowjose2000@gmail.com>
2022-08-13 23:12:50 -0500

    update form donation

---
+++ assets/js/form-donation.js
@@ -0,0 +1,41 @@
+//script de la API email js
+const btn = document.getElementById('button');
+
+document.getElementById('form').addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando Espere...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_a7q2a7m';
+
+   emailjs.sendForm(serviceID, templateID, this).then(() => {
+   //esta linea se ejecuta despues de enviar el form para despues limpiar el form y aparecer la alerta
+   document.getElementById("form").reset();
+      btn.value = 'Send Email';
+         //sweetAlert
+         Swal.fire({
+          icon: 'success',
+          title: 'Muchas gracias por tu apoyo',
+          footer:'<span class="color-alert-footer">Con tu ayuda, contribuimos con la continuidad de los proyectos de vida de nuestros niños y jonvenes y la tranquilidad de sus familias</span>',
+          timerProgressBar:'true',
+          timer: 7000,
+          showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+          }
+        });
+    }, (_err) => {
+      btn.value = 'Send Email';
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Error al enviar tu mensaje, vuelve a intentar!',
+        showConfirmButton:true,
+        ConfirmButtonText: "Salir",
+        // footer: '<a href="">Error al enviar tu mensaje</a>'
+      });
+    });
+});
